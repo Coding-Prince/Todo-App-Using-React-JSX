6c41a7ed8eee18c2b02c856cbf0723f5b1304509
Coding-Prince <programmingprince2008@gmail.com>
2024-04-04 18:02:50 -0700

    Add Delete Button on each todo item

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,8 +3,13 @@ import styles from "./todoitem.module.css";
 export default function TodoItem({ item }) {
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{item}</div>
-      <hr className={styles.line}/>
+      <div className={styles.itemName}>
+        {item}
+        <span>
+          <button className={styles.deleteButton}>⛔</button>
+        </span>
+      </div>
+      <hr className={styles.line} />
     </div>
   );
 }
