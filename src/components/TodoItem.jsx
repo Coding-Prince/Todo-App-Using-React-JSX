26223e7eae61104e1125a2f31c0118f04a4a8e1c
Coding-Prince <programmingprince2008@gmail.com>
2024-04-08 12:18:45 -0700

    Add Function to Mark a todo item completed

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,17 +1,29 @@
+//Imported component, modules, hooks ets
 import styles from "./todoitem.module.css";
 
 export default function TodoItem({ item, todos, setTodos }) {
-
   //"Utilizing state variable 'todos' and 'setTodos' function to delete items based on a condition."
   function handleDelete(item) {
     setTodos(todos.filter((todo) => todo !== item));
-    console.log(item)
   }
+
+  //Function to handle click events and toggle the 'done' property of a todo item
+  function handleClick(name) {
+    setTodos(
+      todos.map((todo) =>
+        todo.name === name ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  }
+
+  const dashedLine = item.done ? styles.completed : "";
 
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item.name}
+        <span className={dashedLine} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
         <span>
           <button
             onClick={() => handleDelete(item)}
