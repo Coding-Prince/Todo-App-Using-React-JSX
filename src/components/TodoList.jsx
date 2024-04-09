4152ed068466b66c71e44ed343c022cbb08ce7a1
Coding-Prince <programmingprince2008@gmail.com>
2024-04-09 12:04:10 -0700

    Add Sorting Todo Item Functionality

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,11 +2,19 @@ import TodoItem from "./TodoItem";
 import style from "./todolist.module.css";
 
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodos = todos
+    .slice()
+    .sort((a, b) => Number(a.done) - Number(b.done));
   return (
     <div className={style.list}>
       {/* Displaying Each Todo Item */}
-      {todos.map((item) => (
-        <TodoItem key={item.name} item={item} todos={todos} setTodos={setTodos} />
+      {sortedTodos.map((item) => (
+        <TodoItem
+          key={item.name}
+          item={item}
+          todos={todos}
+          setTodos={setTodos}
+        />
       ))}
     </div>
   );
